1016f219a4dad5c9bc1bac57df298a21c4ed89da
Yui <xtypebb@163.com>
2022-05-24 20:47:52 +0800

    111

---
+++ db/config.js
@@ -0,0 +1,21 @@
+let dbOption
+
+// dbOption = {
+//   connectionLimit: 10,
+//   host: 'localhost',
+//   user: 'root',
+//   password: '123456789',
+//   port: '3306',
+//   database: 'xd_blog'
+// }
+
+dbOption = {
+  connectionLimit: 10,
+  host: '192.168.169.128',
+  user: 'root',
+  password: 'Xdclass123',
+  port: '3306',
+  database: 'xd_blog'
+}
+
+module.exports = dbOption
